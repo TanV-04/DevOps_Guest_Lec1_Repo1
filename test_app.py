c7ba150ed79017bc199e7263b09f7954de71955c
TanV-04 <aretanvi@gmail.com>
2025-09-13 17:42:19 +0530

    comments

--- test_app.py
+++ test_app.py
@@ -2,3 +2,8 @@ from app import add
 
 def test_add():
     assert add(2, 3) == 5
+
+#this is a unit test function
+# pytest looks for any function that starts with test_.
+# If add(2, 3) equals 5, the test passes.
+# If not, the test fails and pytest reports an error.
